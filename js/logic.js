622ffb5f08aa261a63d3fe323ef9b26c6fc240bf
Juan-Code-Gomez <jgomezberrio054@gmail.com>
2021-12-04 18:42:57 -0500

    Se agrega un poco de diseño y se agrega mas logica para el resultado

--- js/logic.js
+++ js/logic.js
@@ -1,69 +1,87 @@
-
-let hand2 = "Piedra";
-let hand3 = "Tijera";
-
+let selectPlayer = "Esperando Elección";
+let selectCpu = "Esperando Elección";
 
 
+document.getElementById('tuEleccion').innerHTML = selectPlayer
+document.getElementById('eleccionCpu').innerHTML = selectCpu
 
 
 function hand1Funtion(){
+    let cpu = "";
     let hand1 = "Papel";
     let num = Math.floor((Math.random()*3));
-    console.log(num); 
-    arrayHandUser = ["Papel", "Piedra", "Tijera"];
+    if (num == 1) {
+        cpu= "Papel";
+    }else if(num == 2){
+        cpu ="Piedra";
+    }else{
+        cpu = "Tijera";
+    }
+    console.log(cpu);
     console.log(hand1);
-    // if(hand1 ==="Papel" && cpu ==="Piedra"){
-    //     alert("Ganaste contra la maquina");
-    // }else if(hand1==="Papel" && cpu==="Tijera"){
-    //     alert("Perdiste contra la maquina");
-    // }else if(hand1==="Papel" && cpu==="Papel"){
-    //     console.log("Es un empate");
-    // }else{
-    //     console.log("ingrese un valor correcto");
-    // }
-        
-    // }else if(user==="Piedra" && cpu==="Papel"){
-    //     console.log("Perdiste contra la maquina");
-    // }else if(user==="Piedra" && cpu==="Piedra"){
-    //     console.log("Es un empate");
-    // }else if(user==="Piedra" && cpu==="Tijera"){
-    //     console.log("Ganaste contra la maquina");
-    // }else if(user==="Tijera" && cpu==="Papel"){
-    //     console.log("Ganaste contra la maquina");
-    // }else if(user==="Tijera" && cpu==="Piedra"){
-    //     console.log("Perdiste contra la maquina");
-    // }else if(user==="Tijera" && cpu==="Tijera"){
-    //     console.log("Es un empate");
-    // }else{
-    //     console.log("Ingresa un valor correcto");
-    // }
-}
-
-function rockPaperScissors(){
-
-    // console.log(user, cpu);
-
     if(hand1 ==="Papel" && cpu ==="Piedra"){
         alert("Ganaste contra la maquina");
+        document.getElementById('tuEleccion').innerHTML = "Escogiste Papel"
+        document.getElementById('eleccionCpu').innerHTML = "La maquina escogio " + cpu;
     }else if(hand1==="Papel" && cpu==="Tijera"){
         alert("Perdiste contra la maquina");
+        document.getElementById('tuEleccion').innerHTML = "Escogiste Papel"
+        document.getElementById('eleccionCpu').innerHTML = "La maquina escogio " + cpu;
     }else if(hand1==="Papel" && cpu==="Papel"){
-        console.log("Es un empate");
-    }else if(user==="Piedra" && cpu==="Papel"){
-        console.log("Perdiste contra la maquina");
-    }else if(user==="Piedra" && cpu==="Piedra"){
-        console.log("Es un empate");
-    }else if(user==="Piedra" && cpu==="Tijera"){
-        console.log("Ganaste contra la maquina");
-    }else if(user==="Tijera" && cpu==="Papel"){
-        console.log("Ganaste contra la maquina");
-    }else if(user==="Tijera" && cpu==="Piedra"){
-        console.log("Perdiste contra la maquina");
-    }else if(user==="Tijera" && cpu==="Tijera"){
-        console.log("Es un empate");
-    }else{
-        console.log("Ingresa un valor correcto");
+        alert("Es un empate");
+        document.getElementById('tuEleccion').innerHTML = "Escogiste Papel"
+        document.getElementById('eleccionCpu').innerHTML = "La maquina escogio " + cpu;
     }
-} 
+}
 
-// rockPaperScissors(hand1,hand2);
+function hand2Funtion(){
+    let cpu = "";
+    let hand2 = "Piedra";
+    let num = Math.floor((Math.random()*3));
+    if (num == 1) {
+        cpu= "Papel";
+    }else if(num == 2){
+        cpu ="Piedra";
+    }else{
+        cpu = "Tijera";
+    }
+    console.log(cpu);
+    console.log(hand2);
+    if(hand2==="Piedra" && cpu==="Papel"){
+        alert("Perdiste contra la maquina");
+    }else if(hand2==="Piedra" && cpu==="Piedra"){
+        alert("Es un empate");
+    }else if(hand2==="Piedra" && cpu==="Tijera"){
+        alert("Ganaste contra la maquina");
+    }else if(hand2==="Tijera" && cpu==="Papel"){
+        alert("Ganaste contra la maquina");
+    }else if(hand2==="Tijera" && cpu==="Piedra"){
+        alert("Perdiste contra la maquina");
+    }else if(hand2==="Tijera" && cpu==="Tijera"){
+        alert("Es un empate");
+    }
+}
+
+function hand3Funtion(){
+    let cpu = "";
+    let hand3 = "Tijera";
+    let num = Math.floor((Math.random()*3));
+    if (num == 1) {
+        cpu= "Papel";
+    }else if(num == 2){
+        cpu ="Piedra";
+    }else{
+        cpu = "Tijera";
+    }
+    console.log(cpu);
+    console.log(hand3);
+    if(hand3==="Tijera" && cpu==="Papel"){
+        alert("Ganaste contra la maquina");
+    }else if(hand3==="Tijera" && cpu==="Piedra"){
+        alert("Perdiste contra la maquina");
+    }else if(hand3==="Tijera" && cpu==="Tijera"){
+        alert("Es un empate");
+    }
+}
+
+
